577448e0dbdbf47a69729bc3bfb79d7c91407ae0
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-01-18 21:56:56 +0100

    App updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import TeacherList from "./components/TeacherList";
 import FavoritePage from "./components/FavoritePage";
+import AuthForm from "./components/AuthForm";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={HomePage} />
+        <Route path="/login" component={AuthForm} />
         <Route path="/teachers" component={TeacherList} />
         <Route path="/favorites" component={FavoritePage} />
       </Switch>
