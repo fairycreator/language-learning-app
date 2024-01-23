00e3c2bb98070126eb917c0c82c485f2674d9833
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-01-23 18:24:41 +0100

    teacher details modal updated

--- src/components/Modals/TeacherDeatilsModal.js
+++ src/components/Modals/TeacherDeatilsModal.js
@@ -3,6 +3,15 @@ import React from "react";
 const TeacherDetailsModal = ({ teacher, onClose }) => {
   return (
     <div className="modal">
+      <img
+        src={teacher.avatar_url}
+        alt={`${teacher.name} ${teacher.surname}`}
+      />
+      <h3>
+        {teacher.name} {teacher.surname}
+      </h3>
+      <p>{teacher.bio}</p>
+      {/* Display other details and reviews */}
       <button onClick={onClose}>Close</button>
     </div>
   );
