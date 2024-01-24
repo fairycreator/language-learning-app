7a6fc6349c099f23e694621bd179d4e12fbc3d50
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-01-24 21:19:56 +0100

    teacher filter updated

--- src/components/TeacherFilter/TeacherFilter.js
+++ src/components/TeacherFilter/TeacherFilter.js
@@ -1,31 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsChevronDown } from "react-icons/bs";
+import { nanoid } from "@reduxjs/toolkit";
+import getUniqueValues from "../../helpers/getUniqueValues";
+import "./filter.css";
 
-const TeacherFilter = ({ onFilterChange }) => {
-  const handleFilterChange = (event) => {
-    const { name, value } = event.target;
-    onFilterChange(name, value);
-  };
+const Dropdown = ({ title, items, onSelect, selectedValue }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <form>
-      <label>
-        Language:
-        <select name="language" onChange={handleFilterChange}>
-          <option value="">Any</option>
-          <option value="">English</option>
-          <option value="">Ukrainian</option>
-        </select>
-      </label>
-      <label>
-        Price:
-        <select name="language" onChange={handleFilterChange}>
-          <option value="">Any</option>
-          <option value="">Low</option>
-          <option value="">High</option>
-        </select>
-      </label>
-    </form>
+    <div className="filterBox">
+      <p className="filterTitle">{title}</p>
+      <div onClick={() => setIsOpen(!isOpen)} className="filterInputBox">
+        {selectedValue}
+        <button type="button" className="openListBtn">
+          <BsChevronDown size={20} />
+        </button>
+        <ul
+          className={
+            isOpen ? "dropDowList active" : "dropDowList visually-hidden"
+          }
+        >
+          <li onClick={() => onSelect(false)}>skip...</li>
+          {items.map((el) => (
+            <li key={nanoid()} onClick={() => onSelect(el)}>
+              {el}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
-export default TeacherFilter;
+const Filter = ({
+  teachers,
+  setLanguageFromFilter,
+  setLevelFromFilter,
+  setPriceFromFilter,
+}) => {
+  const [selectedLanguage, setSelectedLanguage] = useState("Choose a value");
+  const [selectedLevel, setSelectedLevel] = useState("Choose a value");
+  const [selectedPrice, setSelectedPrice] = useState("Choose a value");
+
+  const uniqueLanguages = getUniqueValues(teachers, "languages");
+  const uniqueLevels = getUniqueValues(teachers, "levels");
+  const uniquePrices = getUniqueValues(teachers, "price_per_hour");
+
+  return (
+    <div className="FilterMainBox">
+      <Dropdown
+        title="Languages"
+        items={uniqueLanguages}
+        onSelect={(value) => {
+          setSelectedLanguage(value || "Choose a value");
+          setLanguageFromFilter(value);
+        }}
+        selectedValue={selectedLanguage}
+      />
+      <Dropdown
+        title="Level of Knowledge"
+        items={uniqueLevels}
+        onSelect={(value) => {
+          setSelectedLevel(value || "Choose a value");
+          setLevelFromFilter(value);
+        }}
+        selectedValue={selectedLevel}
+      />
+      <Dropdown
+        title="Price"
+        items={uniquePrices}
+        onSelect={(value) => {
+          setSelectedPrice(value ? `${value} $` : "Choose a value");
+          setPriceFromFilter(value);
+        }}
+        selectedValue={selectedPrice}
+      />
+    </div>
+  );
+};
+
+export default Filter;
