46d23abb5e324bb98920ec31a0a713b78212f949
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-02-06 10:33:35 +0100

    teachers filter updated

--- src/components/TeacherFilter/TeacherFilter.js
+++ src/components/TeacherFilter/TeacherFilter.js
@@ -4,77 +4,173 @@ import { nanoid } from "@reduxjs/toolkit";
 import getUniqueValues from "../../helpers/getUniqueValues";
 import "./TeacherFilter.css";
 
-const Dropdown = ({ title, items, onSelect, selectedValue }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div className="filterBox">
-      <p className="filterTitle">{title}</p>
-      <div onClick={() => setIsOpen(!isOpen)} className="filterInputBox">
-        {selectedValue}
-        <button type="button" className="openListBtn">
-          <BsChevronDown size={20} />
-        </button>
-        <ul
-          className={
-            isOpen ? "dropDowList active" : "dropDowList visually-hidden"
-          }
-        >
-          <li onClick={() => onSelect(false)}>skip...</li>
-          {items.map((el) => (
-            <li key={nanoid()} onClick={() => onSelect(el)}>
-              {el}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
 const Filter = ({
   teachers,
   setLanguageFromFilter,
   setLevelFromFilter,
   setPriceFromFilter,
 }) => {
-  const [selectedLanguage, setSelectedLanguage] = useState("Choose a value");
-  const [selectedLevel, setSelectedLevel] = useState("Choose a value");
-  const [selectedPrice, setSelectedPrice] = useState("Choose a value");
+  const [isLanguagesOpen, setIsLanguagesOpen] = useState(false);
+  const [isLevelOpen, setIsLevelOpen] = useState(false);
+  const [isPriceOpen, setIsPriceOpen] = useState(false);
 
-  const uniqueLanguages = getUniqueValues(teachers, "languages");
-  const uniqueLevels = getUniqueValues(teachers, "levels");
-  const uniquePrices = getUniqueValues(teachers, "price_per_hour");
+  const [selectedLanguage, setSelectedLanguage] = useState("Сhoose a value");
+  const [selectedLevels, setSelectedLevels] = useState("Сhoose a value");
+  const [selectedPrice, setSelectedPrice] = useState("Сhoose a value");
+
+  const uniqueLanguagesArray = getUniqueValues(teachers, "languages");
+  const uniqueLevelsArray = getUniqueValues(teachers, "levels");
+  const uniquePriceArray = getUniqueValues(teachers, "price_per_hour");
+
+  const handleOpenList = (name) => {
+    switch (name) {
+      case "languages":
+        setIsLanguagesOpen(!isLanguagesOpen);
+        break;
+      case "level":
+        setIsLevelOpen(!isLevelOpen);
+        break;
+      case "price":
+        setIsPriceOpen(!isPriceOpen);
+        break;
+
+      default:
+        break;
+    }
+  };
 
   return (
     <div className="FilterMainBox">
-      <Dropdown
-        title="Languages"
-        items={uniqueLanguages}
-        onSelect={(value) => {
-          setSelectedLanguage(value || "Choose a value");
-          setLanguageFromFilter(value);
-        }}
-        selectedValue={selectedLanguage}
-      />
-      <Dropdown
-        title="Level of Knowledge"
-        items={uniqueLevels}
-        onSelect={(value) => {
-          setSelectedLevel(value || "Choose a value");
-          setLevelFromFilter(value);
-        }}
-        selectedValue={selectedLevel}
-      />
-      <Dropdown
-        title="Price"
-        items={uniquePrices}
-        onSelect={(value) => {
-          setSelectedPrice(value ? `${value} $` : "Choose a value");
-          setPriceFromFilter(value);
-        }}
-        selectedValue={selectedPrice}
-      />
+      <div className="filterBox">
+        <p className="filterTitle">Languages</p>
+        <div
+          onClick={() => handleOpenList("languages")}
+          className="filterInputBox"
+          style={{ minWidth: "221px", cursor: "pointer" }}
+        >
+          {selectedLanguage}
+          <button name="languages" type="button" className="openListBtn">
+            <BsChevronDown name="languages" size={20} />
+          </button>
+
+          <ul
+            className={
+              isLanguagesOpen
+                ? "dropDowList active"
+                : "dropDowList visually-hidden"
+            }
+          >
+            <li
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setSelectedLanguage("Сhoose a value");
+                setLanguageFromFilter(false);
+                setIsLanguagesOpen(false);
+              }}
+            >
+              skip...
+            </li>
+            {uniqueLanguagesArray.map((el) => (
+              <li
+                key={nanoid()}
+                style={{ cursor: "pointer" }}
+                onClick={(e) => {
+                  setSelectedLanguage(e.target.innerText);
+                  setLanguageFromFilter(e.target.innerText);
+                  setIsLanguagesOpen(false);
+                }}
+              >
+                {el}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="filterBox">
+        <p className="filterTitle">Level of knowledge</p>
+        <div
+          onClick={() => handleOpenList("level")}
+          className="filterInputBox"
+          style={{ minWidth: "198px", cursor: "pointer" }}
+        >
+          {selectedLevels}
+          <button name="level" type="button" className="openListBtn">
+            <BsChevronDown size={20} />
+          </button>
+
+          <ul
+            className={
+              isLevelOpen ? "dropDowList active" : "dropDowList visually-hidden"
+            }
+          >
+            <li
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setSelectedLevels("Сhoose a value");
+                setLevelFromFilter(false);
+                setIsLevelOpen(false);
+              }}
+            >
+              skip...
+            </li>
+            {uniqueLevelsArray.map((el) => (
+              <li
+                key={nanoid()}
+                style={{ cursor: "pointer" }}
+                onClick={(e) => {
+                  setSelectedLevels(e.target.innerText);
+                  setLevelFromFilter(e.target.innerText);
+                  setIsLevelOpen(false);
+                }}
+              >
+                {el}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="filterBox">
+        <p className="filterTitle">Price</p>
+        <div
+          onClick={() => handleOpenList("price")}
+          className="filterInputBox"
+          style={{ minWidth: "124px", cursor: "pointer" }}
+        >
+          {selectedPrice} $
+          <button name="price" type="button" className="openListBtn">
+            <BsChevronDown size={20} />
+          </button>
+          <ul
+            className={
+              isPriceOpen ? "dropDowList active" : "dropDowList visually-hidden"
+            }
+          >
+            <li
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                setSelectedPrice("Сhoose a value");
+                setPriceFromFilter(false);
+                setIsPriceOpen(false);
+              }}
+            >
+              skip...
+            </li>
+            {uniquePriceArray.map((el) => (
+              <li
+                key={nanoid()}
+                style={{ cursor: "pointer" }}
+                onClick={(e) => {
+                  setSelectedPrice(e.target.innerText);
+                  setPriceFromFilter(e.target.innerText);
+                  setIsPriceOpen(false);
+                }}
+              >
+                {el}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
